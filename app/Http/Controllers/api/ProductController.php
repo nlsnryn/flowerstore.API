963e61006f218ff67cb4192a2401b97b14c1fe98
NelsonRyan-06 <nelsonarabit00@gmail.com>
2023-07-05 23:16:10 +0800

    add Readme

--- app/Http/Controllers/api/ProductController.php
+++ app/Http/Controllers/api/ProductController.php
@@ -3,16 +3,35 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
+        $products = Product::all();
+        return response()->json($products);
     }
 
-    public function store()
+    public function store(Request $request)
     {
+        $request->validate([
+            'product_name' => 'required|max:255|string',
+            'product_description' => 'required|max:255|string',
+            'quantity' => 'required|max:255',
+            'price' => 'required|max:255',
+        ]);
+
+        $products = Product::create([
+            'product_name' => $request->product_name,
+            'product_description' => $request->product_description,
+            'quantity' => $request->quantity,
+            'price' => $request->price,
+            'status' => true
+        ]);
+
+        return response()->json($products);
     }
 
     public function show()
